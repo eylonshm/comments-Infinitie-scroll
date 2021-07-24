bae170b376e072b557ff7a166a229e5b72cca89d
Eylon Shmilovich <eylonshm@gmail.com>
2021-07-24 11:55:37 +0300

    Create comment component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css'
+import colors from './assets/colors/colors'
+import Comment from './components/comment'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div style={styles.appWrapper}>
+            <Comment
+                comment={{
+                    email: 'email@gmail.com',
+                    name: 'lorem ipsum jfofof okjfiui',
+                    body: 'dsjdosajdiosaj doai jdosia jdsoai djsoai djsaoi djsaoi djsaoid sajodi asjio',
+                }}
+            />
+        </div>
+    )
 }
 
 export default App
+
+const styles = {
+    appWrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'Montserrat',
+    },
+}
---
+++ src/components/comment.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import colors from '../assets/colors/colors'
+
+const Comment = ({ comment }) => {
+    return (
+        <div style={styles.commentWrapper}>
+            <p style={styles.email}>{comment.email}</p>
+            <h3 style={styles.name}>{comment.name}</h3>
+            <p>{comment.body}</p>
+        </div>
+    )
+}
+
+export default Comment
+
+const styles = {
+    commentWrapper: {
+        marginTop: '20px',
+        width: '60%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '15px',
+        overflow: 'hidden',
+        borderRadius: '10px',
+        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+    },
+    name: {
+        margin: '0 0 20px 0',
+        fontWeight: '600',
+    },
+    email: {
+        marginBottom: '10px',
+        fontSize: '13px',
+    },
+}
